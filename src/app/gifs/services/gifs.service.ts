76267f90fd56681247ec24604ac8fde2a666fe5b
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-10-22 14:58:36 -0300

    listado de busquedas

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+
+  constructor() { }
+
+  get tagsHistory(): string[] {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory( tag: string ) {
+    tag = tag.toLocaleLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag( tag: string ): void {
+    if (!tag) return;
+
+    this.organizeHistory(tag);
+  }
+}
