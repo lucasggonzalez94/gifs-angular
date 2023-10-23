74400d49f2ad590415b365a0bd2c16491aa06f73
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-10-23 09:55:18 -0300

    spinner de carga imagenes

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+  ngOnInit(): void {
+    if (!this.url) throw new Error('url property is required.');
+  }
+  @Input()
+  public url!: string;
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  onLoad(): void {
+    this.hasLoaded = true;
+  }
+}
